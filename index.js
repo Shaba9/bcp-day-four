d58ca4379d62933294a7e6191e60e02848432b7e
Shaba9 <shaba92@gmail.com>
2018-10-05 12:54:53 -0700

    pizza app

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 function handleSubmit(event) {
     var form = event.target;
     var elements = form.elements;
-    var guess = elements.guess.value;
+    var top = document.querySelectorAll('input[name="toppings"]:checked');
+    var toppings = [];
+    var order = {};
 
-    var result = document.getElementById('result');
-    // var name = elements.name.value;
-    // console.log('name', name);
-
-    // var comments = elements.name.value;
-    // console.log('comments', comments);
-
-    // var toppings = elements.name.value;
-    // console.log('toppings', toppings);
-
-    // var grade = elements.name.value;
-    // console.log('grade', grade);
-
-
-    
-
-    // result.textContent = 'You made a guess of ' + guess;
-
-    console.log(guess);
-    
-    var correct = 7;
-
-    if (guess == correct){
-        result.textContent = 'Correct';
-    } else if(guess < correct){
-        result.textContent = 'Too low';
-    }else {
-        result.textContent = 'Too high';
+    for(var i = 0; i < top.length; i++){
+        toppings.push(top[i].value);
     }
 
-    console.log(elements.grade.value);
-    console.log(elements.toppings.slice(0,3).value);
+    order['Topppings'] = toppings;
+    order['Crust'] = elements.crust.value;
+    order['Special Instructions'] = elements.instructions.value;
+    
+    console.log(order);
 }
